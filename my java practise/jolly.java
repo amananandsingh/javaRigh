da91f6f21fdaf69c112a5b4f68e3cb069d7c13a3
amananandsingh <amanrathore8178@gmail.com>
2023-11-21 19:36:37 +0530

    hi

---
+++ my java practise/jolly.java
@@ -0,0 +1,10 @@
+public class jolly {
+    public static void main(String[] args) {
+        System.out.println("how are you ?");
+        int a =0;
+        int b=9;
+        for(a=0;a<=b;a++){
+        System.out.println("I am FINE");
+    }
+    }
+}
